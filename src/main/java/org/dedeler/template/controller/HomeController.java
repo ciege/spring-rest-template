cb1069c51842795cfb327660cceffe4f8573367e
Destan Sarpkaya <destan@dorukdestan.com>
2012-10-14 22:43:22 +0300

    restores logged annotation

--- src/main/java/org/dedeler/template/controller/HomeController.java
+++ src/main/java/org/dedeler/template/controller/HomeController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import org.dedeler.template.annotation.Logged;
 import org.dedeler.template.context.MessageHelper;
 import org.dedeler.template.exception.ErrorCode;
+import org.dedeler.template.service.LoggingService.LogLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,8 +20,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Handles requests for the application home page.
  */
+@Logged(level = LogLevel.CONTROLLER)
 @Controller
-public class HomeController extends AbstractController{
+public class HomeController extends AbstractController {
 
 	@Value("${test.message}")
 	private String test;
@@ -41,7 +44,7 @@ public class HomeController extends AbstractController{
 		list.add(MessageHelper.getMessage(ErrorCode.USER_NOT_ACTIVATED, locale));
 
 		return list;
-		
+
 	}
 
 }
