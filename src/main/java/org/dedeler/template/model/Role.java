266bdc287a0bf5752bb2eb349d5fda88409bda3f
Yasa Akbulut <yasakbulut@gmail.com>
2012-11-17 20:51:32 +0200

    comments fetch type choice for Role.privileges

--- src/main/java/org/dedeler/template/model/Role.java
+++ src/main/java/org/dedeler/template/model/Role.java
@@ -9,6 +9,11 @@ import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * Represents a role. 
+ * @author yasa
+ *
+ */
 @Entity
 @Table(name = "TemplateRole")
 public class Role extends AbstractModel implements GrantedAuthority {
@@ -16,6 +21,12 @@ public class Role extends AbstractModel implements GrantedAuthority {
 	private static final long serialVersionUID = 1L;
 	private String name;
 
+	/**
+	 * We'll almost always need the privilege list
+	 * initialized, so we set the fetch type to eager.
+	 * TemplatePermissionEvaluator#hasPermission relies
+	 * on this. 
+	 */
 	@OneToMany(fetch = FetchType.EAGER)
 	private List<Privilege> privileges;
 
