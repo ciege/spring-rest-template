25362eb0b09ce406de2fe95c2c9b11f2269ff6f1
cj rogers <c.j.rogers@gmail.com>
2018-10-16 11:47:12 -0400

    Update Logged.java

--- src/main/java/org/dedeler/template/annotation/Logged.java
+++ src/main/java/org/dedeler/template/annotation/Logged.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 import org.dedeler.template.service.LoggingService.LogLevel;
 import org.dedeler.template.service.LoggingService.LogType;
 
